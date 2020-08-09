2b8e989caa41c0711349de46267035290bf5343e
fiekong <feikong>
2020-08-09 14:34:34 +0800

    [update] log func succ add a param

--- sx_code/log.py
+++ sx_code/log.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-def log_do_func_succ(func, log_func):
-    def wrapper(*args, **kw):
-        # log_func('call %s():' % func.__name__)
-        log_func(f"call {func.__name__}({args}, {kw}) successfully.")
-        return func(*args, **kw)
-    return wrapper
+def log_do_func_succ(log_func):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            log_func(f"call {func.__name__}({args}, {kw}) successfully.")
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 from sx_code import funny
 
 funny.funny("aaaa")
+
+
+from sx_code.log import log_do_func_succ
+
+
+@log_do_func_succ(print)
+def add(x, y):
+    print(x + y)
+
+    add(1,2)
+
+
+if __name__ == '__main__':
+    add(1, 2)
