472b7e87679300952947c8cd5e81345af8847766
fiekong <feikong>
2020-08-09 14:01:50 +0800

    [update] guandu version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ KEYWORDS = "keyword"
 AUTHOR = "feikong"
 AUTHOR_EMAIL = "feikong@shouxin168.com"
 URL = "http://test.com"
-VERSION = "1.0.4"
+VERSION = "1.0.5"
 LICENSE = "MIT"
 setup(
     name=NAME, version=VERSION,
